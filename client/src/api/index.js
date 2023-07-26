9544f030742e620e93e11fa65ef343ca80aba8c9
kodurimohan <kodurimohan@outlook.com>
2023-07-26 20:08:01 +0530

    renamed env files to REACT_APP_

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const SERVER_URL = process.env.SERVER_URL || "http://localhost:5000";
+const SERVER_URL = process.env.REACT_APP_SERVER_URL || "http://localhost:5000";
 
 const API = axios.create({ baseURL: SERVER_URL });
 
